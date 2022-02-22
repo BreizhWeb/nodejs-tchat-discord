8b824fc1aec5e835ec760d90aef2e3fa5731395e
JulienAballea <julien.aballea@free.fr>
2022-02-22 09:34:37 +0100

    data

---
+++ data.js
@@ -0,0 +1,66 @@
+module.exports = {
+  users: [
+    {
+      id: 0,
+      name: 'Ronan',
+      chans: [0, 1]
+    },
+    {
+      id: 1,
+      name: 'Thomas',
+      chans: [0]
+    },
+    {
+      id: 2,
+      name: 'Nicolas',
+      chans: [1]
+    },
+    {
+      id: 3,
+      name: 'Agénor',
+      chans: [0, 1]
+    },
+    {
+      id: 4,
+      name: 'Julien',
+      chans: [0, 1]
+    },
+    {
+      id: 5,
+      name: 'Annaeg',
+      chans: [0, 1]
+    },
+    {
+      id: 6,
+      name: 'Gwen',
+      chans: [0, 1]
+    },
+    {
+      id: 7,
+      name: 'Artur',
+      chans: [0, 1]
+    },
+    {
+      id: 8,
+      name: 'Dorian',
+      chans: [0, 1]
+    },
+    {
+      id: 9,
+      name: 'Elouan',
+      chans: [0, 1]
+    }
+  ],
+  Rooms: [
+    {
+      id: 0,
+      name: 'MDS',
+      users: [],
+    },
+    {
+      id: 1,
+      name: 'Privé',
+      users: [],
+    },
+  ]
+}
