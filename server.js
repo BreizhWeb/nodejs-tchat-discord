0de8b8430523150a9d1fed190decdd569463da81
Gwenc'hlan LE BRECH <gwenchlan.lebrech@my-digital-school.org>
2022-02-22 17:05:50 +0100

    push 6

--- server.js
+++ server.js
@@ -25,15 +25,16 @@ io.sockets.on("connection",(socket)=>{
     });
 
     //round 2
-    socket.on("new user",function (data,callback){
-        if(usernames.indexOf(data) != -1){
+    socket.on("registration",function (data,callback){
+        console.log(data);
+        /*if(usernames.indexOf(data) != -1){
             callback(false);
         }else{
             callback(true);
             socket.username = data;
             usernames.push(socket.username);
             updateUsernames();
-        }
+        }*/
     });
 
     //Update Usernames
@@ -54,3 +55,29 @@ io.sockets.on("connection",(socket)=>{
 
 })
 
+const jwt = require('jsonwebtoken');
+
+exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if (!user) {
+          return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+        }
+        bcrypt.compare(req.body.password, user.password)
+          .then(valid => {
+            if (!valid) {
+              return res.status(401).json({ error: 'Mot de passe incorrect !' });
+            }
+            res.status(200).json({
+              userId: user._id,
+              token: jwt.sign(
+                { userId: user._id },
+                'RANDOM_TOKEN_SECRET',
+                { expiresIn: '24h' }
+              )
+            });
+          })
+          .catch(error => res.status(500).json({ error }));
+      })
+      .catch(error => res.status(500).json({ error }));
+  };
