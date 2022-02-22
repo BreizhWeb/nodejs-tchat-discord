f866257860bf69a1e8af30191786cc2de1760566
Agenor Houessou <agenor.houessou@hotmail.fr>
2022-02-22 09:58:35 +0100

    installations des dépendances

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
+require("dotenv").config();
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
 server= require("http").createServer(app);
 const io = require('socket.io')(server);
+const fs = require('fs');
 let usernames = [];
+
+const mysql = require('mysql');
+app.use(bodyParser.json());
+
+//création d'une connection a la bdd
+const con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER_NAME,
+    password:process.env.DB_USER_PASSWORD,
+    database: process.env.DB_NAME
+});
 
 app.use(express.static('public'))
 
@@ -10,9 +24,14 @@ app.get("/",(req,res)=>{
     res.sendFile(__dirname + "/public/index.html");
 })
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => {
-    console.log('le serveur ecoute sur le port %d', PORT);
+    console.log(`le serveur écoute sur le port ${PORT}`);
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connecté à la base de données MySQL!");
 });
 
 io.sockets.on("connection",(socket)=>{
@@ -21,6 +40,9 @@ io.sockets.on("connection",(socket)=>{
     socket.on("send message",(data)=>{
         //round 2 add username
         io.sockets.emit('new message',{msg:data,user:socket.username});
+        
+        //On insere le message dans la bdd
+         insertMsg(socket.username, data);
         console.log(data);
     });
 
