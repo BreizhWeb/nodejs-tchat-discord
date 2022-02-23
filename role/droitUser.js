65ad38381081cf399c0096fa54291680c15b56f1
Dorian Haupas <dorian.haupas@live.fr>
2022-02-23 09:17:57 +0100

    droit user 2

--- role/droitUser.js
+++ role/droitUser.js
@@ -1,10 +1,15 @@
 const { roles } = require('../modules/data')
 
-function verifierDroit(user, role, room) {
-  return (
-    user.role === roles.ADMIN ||
-    project.userId === user.id
-  )
+function verifierDroit(user, role, action) {
+  if(user.role === roles.ADMIN){
+    return true
+  }
+  else if(user.role === roles.USER){
+    return true
+  }
+  else if(user.role === roles.NULL){
+    return false
+  }
 }
 
 function modifierDroit(user, role, room) {
