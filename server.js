012db0d9864fc5d975e9dcfccb1240a26ac190e3
el-elouan <elou50@hotmail.fr>
2022-02-23 11:30:03 +0100

    camarche

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 server= require("http").createServer(app);
 const io = require('socket.io')(server);
 const crypto = require('crypto');
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
 let usernames = [];
 
 app.use(express.urlencoded({ extended:false }));
@@ -14,14 +16,28 @@ app.get("/",(req,res)=>{
 })
 
 app.post("/register",(req,res)=>{
-    res.send(req.body);
+    if (req.body.username == "existe") {
+        res.json({message:"cestok",error:"true"});
+    } else {
+        res.json({message:"cestok",error:"false"});
+    }
+    //res.send(req.body);
     //res.sendFile(__dirname + "/public/index.html");
 })
 
 app.post("/login",(req,res)=>{
+    if (req.body.username == "existe" && req.body.password == "azertyuiop") {
+        res.json({message:"cestok",error:"false"});
+    } else {
+        res.json({message:"cestok",error:"true"});
+    }
     const md5sum = crypto.createHash('md5');
     var password = md5sum.update(req.body.password).digest('hex');
-    res.send(password);
+    /*
+    mettre le token dans le cookie
+    lors du click sur un lien vérifier dans le cookie que le token soit bon (grace a jwt) ou que le hash du mdp dans le cookie corresponde à la bdd
+    puis renvoyer la page
+    */
     //res.sendFile(__dirname + "/public/index.html");
 })
 
