a58103df2a168da066b4a4d12193f36475deef91
Dorian Haupas <dorian.haupas@live.fr>
2022-03-07 16:08:56 +0100

    add server fusion

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const fakedata = require('./modules/fakedata.js')
 const multirooms = require('./modules/multirooms.js')
 const db = require('./db/db.js')
 
+
+
 app.use(express.static('public'))
 
 app.get("/", (req, res) => {
@@ -26,50 +28,41 @@ io.sockets.on("connection", (socket) => {
     } catch (e) {
         console.log(e);
     }
-  });
-  // Send Message
-  socket.on("send message", (data, callback) => {
-    console.log(`[${data.chan}]${socket.user.name} : ${data.msg}`);
-    
-    io.sockets.to(`chan-${data.chan}`).emit('new message', {
-      msg: data.msg,
-      chan: data.chan,
-      user: socket.user.name
-    }, callback())
-  });
-  socket.on("userlist", (data, callback) => {
-      console.log("userlist");
-      callback(fakedata.users.map(u => u.name));
-  });
 
-  socket.on("new user", function (name, callback) {
-      socket.user = fakedata.users.find(v => v.name == name)
-      // If user dont exist
-      if (!socket.user) {
-          callback(false);
-      } else {
-          console.log(`connected : ${name}`);
-          callback(socket.user)
-      }
-  });
-  // Send Message
-  socket.on("send message", (data, callback) => {
-      console.log(`[${data.chan}]${socket.user.name} : ${data.msg}`);
+    socket.on("userlist", (data, callback) => {
+        console.log("userlist");
+        callback(fakedata.users.map(u => u.name));
+    });
 
-      io.to(`chan-${data.chan}`).emit('new message', {
-          msg: data.msg,
-          chan: data.chan,
-          user: socket.user.name
-      }, callback())
-  });
+    socket.on("new user", function (name, callback) {
+        socket.user = fakedata.users.find(v => v.name == name)
+        // If user dont exist
+        if (!socket.user) {
+            callback(false);
+        } else {
+            console.log(`connected : ${name}`);
+            callback(socket.user)
+        }
+    });
+    // Send Message
+    socket.on("send message", (data, callback) => {
+        console.log(`[${data.chan}]${socket.user.name} : ${data.msg}`);
 
-  //Disconnect
-  socket.on("disconnect", function (data) {
-      if (!socket.user?.name) {
-          return;
-      }
-      console.log(`disconnected : ${socket.user?.name}`);
-      multirooms.disconnect(socket.user, io.sockets)
-  });
+        io.to(`chan-${data.chan}`).emit('new message', {
+            msg: data.msg,
+            chan: data.chan,
+            user: socket.user.name
+        }, callback())
+    });
+
+    //Disconnect
+    socket.on("disconnect", function (data) {
+        if (!socket.user?.name) {
+            return;
+        }
+        console.log(`disconnected : ${socket.user?.name}`);
+        multirooms.disconnect(socket.user, io.sockets)
+    });
 
 
+})
