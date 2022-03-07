517558a581aac87f23fc33c0d8e5f006de478a9d
Agenor Houessou <agenor.houessou@hotmail.fr>
2022-03-07 14:02:34 +0100

    new BDD

--- chat_mydi.sql
+++ chat_mydi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 21 fév. 2022 à 14:15
+-- Généré le : lun. 07 mars 2022 à 14:00
 -- Version du serveur : 10.4.22-MariaDB
 -- Version de PHP : 7.4.26
 
@@ -35,6 +35,30 @@ CREATE TABLE `messages` (
   `id_room` int(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Déchargement des données de la table `messages`
+--
+
+INSERT INTO `messages` (`message_id`, `date`, `content`, `id_user`, `id_room`) VALUES
+(1, '2022-02-22', 'test message', 1, 1),
+(2, '2022-02-22', 'Yo !', 3, 2),
+(3, '2022-02-22', 'Yo man la forme ?', 1, 2),
+(4, '2022-02-22', 'Yes et toi ?', 3, 2),
+(5, '2022-02-22', 'Bah écoutes ça va !', 1, 2),
+(6, '2022-02-22', 'Un petit LOL ?', 3, 2),
+(7, '2022-02-22', 'Allez vas-y !', 1, 2),
+(8, '2022-02-22', 'Envoies une invit\' !', 1, 2),
+(9, '2022-02-22', 'Enfaite flemme, y\'a ma go qui vient d\'arriver !', 3, 2),
+(10, '2022-02-22', 'Roooh t\'abuses !', 1, 2),
+(11, '2022-02-22', 'Passes pas le bonjour à ta go du coup ! :P', 1, 2),
+(12, '2022-02-22', 'haha', 3, 2),
+(13, '2022-02-22', 'Allez ciao !', 1, 2),
+(14, '2022-02-22', 'Ciao l\'artiste !', 3, 2),
+(15, '2022-02-22', 'Yo les manos, bien ?', 2, 2),
+(16, '2022-02-22', 'Yes et toi ?', 3, 2),
+(17, '2022-02-22', 'Go Lol !', 1, 2),
+(18, '2022-02-22', 'Allez !', 3, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +69,14 @@ CREATE TABLE `roles` (
   `role_id` int(12) NOT NULL,
   `name` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `roles`
+--
+
+INSERT INTO `roles` (`role_id`, `name`) VALUES
+(1, 'admin'),
+(2, 'modérateur');
 
 -- --------------------------------------------------------
 
@@ -59,6 +91,16 @@ CREATE TABLE `rooms` (
   `Private` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Déchargement des données de la table `rooms`
+--
+
+INSERT INTO `rooms` (`room_id`, `Name`, `Image`, `Private`) VALUES
+(1, 'Test room', '', 0),
+(2, 'Team LoL', '', 0),
+(3, 'Room', '', 1),
+(4, 'test', '', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -72,6 +114,14 @@ CREATE TABLE `rooms_users` (
   `id_role` int(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Déchargement des données de la table `rooms_users`
+--
+
+INSERT INTO `rooms_users` (`rooms_users_id`, `id_room`, `id_user`, `id_role`) VALUES
+(2, 2, 1, 1),
+(3, 2, 3, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -83,6 +133,17 @@ CREATE TABLE `users` (
   `pseudo` varchar(64) NOT NULL,
   `password` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`user_id`, `pseudo`, `password`) VALUES
+(1, 'Rico', 'blabla'),
+(2, 'Koffi', 'yoyo'),
+(3, 'Artur', 'yeye'),
+(4, 'test', ''),
+(5, 'pseudo', 'hgru');
 
 --
 -- Index pour les tables déchargées
@@ -131,31 +192,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT pour la table `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `role_id` int(12) NOT NULL AUTO_INCREMENT;
+  MODIFY `role_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `rooms`
 --
 ALTER TABLE `rooms`
-  MODIFY `room_id` int(12) NOT NULL AUTO_INCREMENT;
+  MODIFY `room_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
 
 --
 -- AUTO_INCREMENT pour la table `rooms_users`
 --
 ALTER TABLE `rooms_users`
-  MODIFY `rooms_users_id` int(12) NOT NULL AUTO_INCREMENT;
+  MODIFY `rooms_users_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(12) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- Contraintes pour les tables déchargées
