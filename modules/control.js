7d4f4b94880efefaa1431df4b1efc12a750b81be
Dorian Haupas <dorian.haupas@live.fr>
2022-04-10 23:08:46 +0200

    ajout de commentaire dodo

--- modules/control.js
+++ modules/control.js
@@ -1,5 +1,6 @@
 /**
  * Réalisé par : Ronan, Julien
+ * Groupe 2, module Role User
  */
 const nodeCache= require('./nodeCache');
 const permission = require('./permissions');
@@ -8,6 +9,13 @@ const multirooms = require('./multirooms.js')
 const logger = require('../log/logger');
 const { io } = require('socket.io-client');
 
+
+/**
+ * Crée un message avec le nom de l'utilsateur et une image
+ * @param {object} socket
+ * @param {object} data
+ * @returns {int}
+ */
 async function createMp(socket, data) {
   let room = await db.rooms.create(data.name + "-" + socket.user.pseudo, data.image, data.private)
   db.roles.create(room.room_id, socket.user.user_id, 2)
@@ -19,6 +27,12 @@ async function createMp(socket, data) {
   return room
 }
 
+/**
+ * Crée la room private ou non entre différents utilisateurs
+ * @param {object} socket
+ * @param {object} data
+ * @returns {int}
+ */
 async function createRoom(socket, data) {
   let room = await db.rooms.create(data.name, data.image, data.private)
   db.roles.create(room.room_id, socket.user.user_id, 0)
@@ -27,6 +41,14 @@ async function createRoom(socket, data) {
   return room
 }
 
+/**
+ * Envoie de message dans une room
+ * @param {object} io
+ * @param {int} user
+ * @param {int} room_id
+ * @param {string} content
+ * @returns {boolean}
+ */
 async function sendMessage(io, user, room_id, content) {
   if (permission.getActionRight(user.user_id, room_id, permission.actions.sendMessage)) {
     let msg_id = await db.messages.create(user.user_id, room_id, content)
@@ -43,6 +65,14 @@ async function sendMessage(io, user, room_id, content) {
   }
 }
 
+/**
+ * Supprimer un message d'un utilisateur dans une room par quelqu'un d'autorisé
+ * @param {object} io
+ * @param {int} user_id
+ * @param {int} room_id
+ * @param {int} msg_id
+ * @returns {boolean}
+ */
 async function deleteMessage(io, user_id, room_id, msg_id) {
   if (permission.getActionRight(user_id, room_id, permission.actions.deleteMessage)) {
     // récupération de l'id du message qui va être supprimer
@@ -58,6 +88,13 @@ async function deleteMessage(io, user_id, room_id, msg_id) {
   }
 }
 
+/**
+ * Rejoindre une room et actualise le pseudo sur le front
+ * @param {int} user_id
+ * @param {int} room_id
+ * @param {string} pseudo
+ * @returns {boolean}
+ */
 async function joinRoom(user_id, room_id , pseudo) { // rajouté le pseudo dans le front
   let room = await db.rooms.select(room_id)
   if (!room.private) {
@@ -69,6 +106,14 @@ async function joinRoom(user_id, room_id , pseudo) { // rajouté le pseudo dans 
   }
 }
 
+/**
+ * Invite un utilisateur à cette room par un utilisateur agréé à le faire
+ * @param {int} user_id
+ * @param {int} room_id
+ * @param {int} invited_user_id
+ * @param {int} invited_user_role=1
+ * @returns {boolean}
+ */
 function inviteUser(user_id, room_id, invited_user_id, invited_user_role = 1) { //
   if (nodeCache.userAlreadyInRoom(room_id,invited_user_id))
     return false
@@ -83,6 +128,13 @@ function inviteUser(user_id, room_id, invited_user_id, invited_user_role = 1) { 
   }
 }
 
+/**
+ * Supprimer un utilisateur de la room
+ * @param {id} user_id
+ * @param {id} room_id
+ * @param {id} deleted_user_id
+ * @returns {boolean}
+ */
 function deleteUser(user_id, room_id, deleted_user_id) {
   if (permission.getActionRight(user_id, room_id, permission.actions.deleteUser)) {
     db.roles.deleteUserFromRoom(deleted_user_id, room_id);
@@ -94,6 +146,12 @@ function deleteUser(user_id, room_id, deleted_user_id) {
   }
 }
 
+/**
+ * Supprime une room
+ * @param {int} user_id
+ * @param {int} room_id
+ * @returns {boolean}
+ */
 async function deleteRoom(user_id, room_id) {
   if (permission.getActionRight(user_id, room_id, permission.actions.deleteRoom)) {
     db.roles.deleteByRoom(room_id);
